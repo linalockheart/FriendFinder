a8d7a15a4f7db61e07ead4613deccebd3c2c0bbc
Lina Lockheart <angelinalockheart@gmail.com>
2018-08-08 19:11:11 -0400

    added html for homepage

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express')
-// var app = express()
- 
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
- 
-// app.listen(3000)
-
 var bodyParser = require('body-parser')
+var app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+ 
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
+
+app.listen(3000);
