067343c1fad423fb55344de7c7ab3f160b9bc327
Lina Lockheart <angelinalockheart@gmail.com>
2018-08-10 15:39:29 -0400

    working on html routes

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+friendsList = [
+    {
+        name: "Lina"
+        photo: "photo"
+        responses: [
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0"
+        ]
+    },
+    {
+        name: "Gab"
+        photo: "photo"
+        responses: [
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0"
+        ]
+    },
+    {
+        name: "Cait"
+        photo: "photo"
+        responses: [
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0"
+        ]
+    },
+    {
+        name: "Jake"
+        photo: "photo"
+        responses: [
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0",
+            "0"
+        ]
+    }
+];
+
+module.exports = friendsList;
