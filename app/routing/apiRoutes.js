7ef97b0e5b8893d2377e71f8bf6f5b8dec282773
Lina Lockheart <angelinalockheart@gmail.com>
2018-08-11 18:11:17 -0400

    almost working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,4 +6,40 @@ module.exports = function (app) {
         res.json(friendList);
     });
 
+    app.post('/api/friendlist', function (req, res) {
+        
+        var bestMatch = {
+            name: "",
+            photo: "",
+            compatibility: 1000
+        };
+
+        console.log(req.body);
+
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        console.log(userScores);
+
+        var totalDifference = 0;
+
+        for (var i = 0; i < frendsList.length; i++) {
+            console.log(friendsList[i]);
+            totalDifference = 0;
+
+            for (var s = 0; s>friends[i].scores[s]; s++) {
+                totalDifference =+ Math.abs(parseInt(userScore[s]) - parseInt(friendsList[i].scores[s]))
+                if (totalDifference <= bestMatch.compatibility) {
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.compatibility = totalDifference;
+                }
+            }
+        }
+
+        friendsList.push(userData);
+        res.json(bestMatch);
+
+    });
+
 }
