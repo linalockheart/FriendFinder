da5bc066aff2858deb1124f1a48f491b631c0cd8
Lina Lockheart <angelinalockheart@gmail.com>
2018-08-11 11:57:44 -0400

    updated friends info

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,66 +1,98 @@
 friendsList = [
     {
-        name: "Lina",
-        photo: "photo",
+        name: "Ross",
+        photo: "https://imgs.6sqft.com/wp-content/uploads/2015/03/21002356/Ross-Gellar.jpg",
         responses: [
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0"
+            "1",
+            "4",
+            "2",
+            "4",
+            "4",
+            "1",
+            "5",
+            "5",
+            "2",
+            "5"
         ]
     },
     {
-        name: "Gab",
-        photo: "photo",
+        name: "Rachel",
+        photo: "http://images2.fanpop.com/images/photos/5300000/Rachel-Green-Friends-tv-characters-5385855-460-346.jpg",
         responses: [
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0"
+            "2",
+            "3",
+            "3",
+            "1",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "3"
         ]
     },
     {
-        name: "Cait",
-        photo: "photo",
+        name: "Chandler",
+        photo: "https://media0ch-a.akamaihd.net/82/99/de4570a567fe8f43e202f9f1aee84bb1.jpg",
         responses: [
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0"
+            "5",
+            "4",
+            "5",
+            "5",
+            "1",
+            "2",
+            "4",
+            "4",
+            "5",
+            "1"
         ]
     },
     {
-        name: "Jake",
-        photo: "photo",
+        name: "Monica",
+        photo: "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimages.hellogiggles.com%2Fuploads%2F2016%2F03%2F02074507%2Fmonica.jpg&w=700&q=85",
         responses: [
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0",
-            "0"
+            "3",
+            "5",
+            "4",
+            "2",
+            "4",
+            "2",
+            "2",
+            "3",
+            "1",
+            "5"
+        ]
+    },
+    {
+        name: "Joey",
+        photo: "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimages.hellogiggles.com%2Fuploads%2F2016%2F07%2F03011013%2Ffriends.jpg&w=700&q=85",
+        responses: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "2",
+            "5",
+            "1",
+            "4",
+            "4",
+            "2"
+        ]
+    },
+    {
+        name: "Phoebe",
+        photo: "http://media.tumblr.com/a5a4651999486da07db13287fa302562/tumblr_inline_mll1ok8Fwk1qz4rgp.jpg",
+        responses: [
+            "4",
+            "2",
+            "5",
+            "3",
+            "3",
+            "4",
+            "1",
+            "5",
+            "3",
+            "1"
         ]
     }
 ];
